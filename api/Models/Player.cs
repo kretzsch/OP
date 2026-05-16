7303c86e3d9da9132d6077e37f88286d9e15e5a2
Thomas Kretzschmar <thomaskretzschmar5@gmail.com>
2026-05-16 23:38:06 +0200

    Models

---
+++ api/Models/Player.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
---
+++ api/Models/Score.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Score
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public int Value { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
