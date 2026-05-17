4eca25871375a37b5e00ed24cd8cce62eea2c147
Thomas Kretzschmar <thomaskretzschmar5@gmail.com>
2026-05-17 23:12:04 +0200

    Added dbcontext

---
+++ api/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Stock> Stocks { get; set; }
+        public DbSet<Models.Comment> Comments { get; set; }  
+    }
+}
