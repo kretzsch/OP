e02dd7622985eac70ffa90a03837e4ba574376b6
Thomas Kretzschmar <thomaskretzschmar5@gmail.com>
2026-05-20 16:52:08 +0200

    ScoreDto and Mapster for score endpoints
    left out datetime, to showcase scoredto working properly.

--- api/Controllers/ScoreController.cs
+++ api/Controllers/ScoreController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos;
+using Mapster;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -22,18 +24,20 @@ namespace api.Controllers
         public IActionResult GetScores()
         {
             var scores = _context.Scores.ToList();
-            return Ok(scores);
+            var response = scores.Adapt<List<Dtos.Score.ScoreDto>>();
+            return Ok(response);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id)
+        public IActionResult GetById(int id)
         {
             var score = _context.Scores.Find(id);
             if (score == null)
             {
                 return NotFound();
             }
-            return Ok(score);
+            var response = score.Adapt<Dtos.Score.ScoreDto>();
+            return Ok(response);
         }
     }
 }
---
+++ api/Dtos/Score/ScoreDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Score
+{
+    public class ScoreDto
+    {
+          public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public int Value { get; set; }
+      
+    }
+}
