ee5812359f22b074ccd051c5d0a2355debc0acfa
Thomas Kretzschmar <thomaskretzschmar5@gmail.com>
2026-05-23 14:38:43 +0200

    maybe actually registering scorerepository would be nice.

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,6 @@
 using api.Data;
+using api.Interfaces;
+using api.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IScoreRepository, ScoreRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
