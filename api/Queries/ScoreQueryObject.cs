3d8c490f65856c59246e0a1d3bae1826ad46800a
Thomas Kretzschmar <thomaskretzschmar5@gmail.com>
2026-05-29 16:32:42 +0200

    Add pagination fields to ScoreQueryObject

--- api/Queries/ScoreQueryObject.cs
+++ api/Queries/ScoreQueryObject.cs
@@ -10,5 +10,7 @@ namespace api.Queries
         public int? PlayerId { get; set; } = null;
         public string? SortBy { get; set; } = null;
         public bool IsDescending { get; set; } = false;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
     }
 }
