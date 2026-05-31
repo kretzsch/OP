f528699143c36c7e66656e0f385eec335161f0be
Thomas Kretzschmar <thomaskretzschmar5@gmail.com>
2026-06-01 00:06:56 +0200

    Add ASP.NET Identity and integrate User model

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -2,16 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models.Auth;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<Models.Auth.User>
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
         }
 
+        /*protected override void OnModelCreating( ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("identity");
+        }*/
 
         public DbSet<Models.Player> Players { get; set; }
         public DbSet<Models.Score> Scores { get; set; }
--- api/Models/Player.cs
+++ api/Models/Player.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models.Auth;
 
 namespace api.Models
 {
@@ -9,6 +10,7 @@ namespace api.Models
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public User User { get; set; } = null!;
     }
 }
